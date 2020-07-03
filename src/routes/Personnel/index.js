1b59384ec724da97bd17a0cb9c51b2d6fe207593
Thobatech <toba@elta.solutions>
2020-07-03 10:21:14 +0100

    Table name fix

--- src/routes/Personnel/index.js
+++ src/routes/Personnel/index.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import Titlebar from "../../components/TitleBar";
 import MenuItem from "../../components/MenuItem/menuItem";
 import { ReactComponent as SearchIcon } from "../../assets/img/search.svg";
-import Table from "./AAA";
+import Table from "./Table";
 // import DepartmentOverlay fro./Overlays/DepartmentOverlaylay";
 // import Add from "./AddPersonnelOverlay";
 import Add from "./Overlays/Add";
