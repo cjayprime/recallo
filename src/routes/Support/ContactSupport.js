69b953e89ac3e04aaba6aeb7e395195be1a59195
Thobatech <toba@elta.solutions>
2020-07-10 09:42:48 +0100

    modification of homepage header, welcome message modal, reset password modal, password succesful modal and business information and FAQs modal page

--- src/routes/Support/ContactSupport.js
+++ src/routes/Support/ContactSupport.js
@@ -10,14 +10,58 @@ class ContactSupport extends Component {
     email: "",
     message: "",
     // error: null,
+    popup: true,
+    show1: false,
+    show2: false,
+    show3: false,
+    show4: false,
+    show5: true,
+    show6: false,
   };
+
+
+
+  toggleDiv = (name) =>{
+    const {popup, show1, show2, show3, show4, show5, show6}= this.state;
+    switch(name) {
+      case "popup":
+       this.setState({popup: !popup})
+       break;
+
+      case "show1":
+       this.setState({show1: !show1})
+       break;
+
+       case "show2":
+       this.setState({show2: !show2})
+       break;
+
+       case "show3":
+       this.setState({show3: !show3})
+       break;
+
+       case "show4":
+       this.setState({show4: !show4})
+       break;
+
+       case "show5":
+       this.setState({show5: !show5})
+       break;
+
+       case "show6":
+       this.setState({show6: !show6})
+       break;
+      default:
+        alert("null");
+   }
+  }
 
   handleChange = (value, name) => {
     this.setState({ [name]: value });
   };
 
   render() {
-    const { firstname, lastname, email, message } = this.state;
+    const { firstname, lastname, email, message, popup, show1,show2, show3,show4,show5,show6  } = this.state;
     return (
       <>
         <div className="support">
@@ -98,6 +142,132 @@ class ContactSupport extends Component {
           </div>
         </div>
         <h5 id="kk" className="light text-light btw">Back to website</h5>
+
+        {popup && 
+                    
+                    <div id="FaqAndTermsDiv" >
+                    <div id="FaqAndTermsDiv2" className="overview br-16 plr-32 mb-32"><br/><br/>
+                    <span onClick={()=>this.toggleDiv("popup")} className="modalHide">x</span>
+                              <div style={{marginTop:40}}>
+                              <span style={{fontSize:20, color:"black", fontWeight:"bold"}}>Frequently Asked Questions</span>
+                              </div>
+                              <br/> <hr/><br/><br/>
+                              
+                              <span className="faqStyletitle" onClick={()=>this.toggleDiv("show1")}>1. WHAT IS RECALLO MOBILE?</span>
+                             {
+                               show1 ?
+                               <div onClick={()=>this.toggleDiv("show1")} className="minus-icon " />
+                               :
+                               <div onClick={()=>this.toggleDiv("show1")} className="plus-icon " />
+
+                             }
+                              
+
+
+                             {show1 &&
+                             <p className="faqStyletext"> provides contact centre services with features as calling, messaging, mailing, calling features, 
+                             and other services to users around the world. Please read our Terms of Service so you understand 
+                             </p>
+                             }
+                            <br/><br/> <hr/> <br/><br/>
+
+                           
+                              <span className="faqStyletitle" onClick={()=>this.toggleDiv("show2")}>2. WHO IS IT FOR?</span>
+                              {
+                               show2 ?
+                               <div onClick={()=>this.toggleDiv("show2")} className="minus-icon " />
+                               :
+                               <div onClick={()=>this.toggleDiv("show2")} className="plus-icon " />
+
+                             }
+                              
+
+
+                             {show2 &&
+                             <p className="faqStyletext"> provides contact centre services with features as calling, messaging, mailing, calling features, 
+                             </p>
+                             }
+                            <br/><br/> <hr/><br/><br/>
+
+                           
+                            <span className="faqStyletitle" onClick={()=>this.toggleDiv("show3")}>3. HOW IS RECALLO MOBILE DIFFERENT FROM ANY OTHER CALL CENTRE APPLICATION?</span>
+                            {
+                               show3 ?
+                               <div onClick={()=>this.toggleDiv("show3")} className="minus-icon " />
+                               :
+                               <div onClick={()=>this.toggleDiv("show3")} className="plus-icon " />
+
+                             }
+
+
+
+                            {show3 &&
+                            <p className="faqStyletext"> provides contact centre services with features as calling, messaging, mailing, calling features, 
+                            </p>
+                            }
+                            <br/><br/> <hr/><br/><br/>
+
+                            <span className="faqStyletitle" onClick={()=>this.toggleDiv("show4")}>4. IS IT AVAILABLE ON MY DEVICE?</span>
+                            {
+                               show4 ?
+                               <div onClick={()=>this.toggleDiv("show4")} className="minus-icon " />
+                               :
+                               <div onClick={()=>this.toggleDiv("show4")} className="plus-icon " />
+
+                             }
+
+
+
+                            {show4 &&
+                            <p className="faqStyletext"> provides contact centre services with features as calling, messaging, mailing, calling features, 
+                            </p>
+                            }
+                            <br/><br/> <hr/><br/><br/>
+
+                            <span className="faqStyletitle" onClick={()=>this.toggleDiv("show5")}>5. WHO ARE THE PEOPLE BEHIND RECALLO MOBILE?</span>
+                            {
+                               show5 ?
+                               <div onClick={()=>this.toggleDiv("show5")} className="minus-icon " />
+                               :
+                               <div onClick={()=>this.toggleDiv("show5")} className="plus-icon " />
+
+                             }
+
+
+
+                            {show5 &&
+                            <p className="faqStyletext"> 
+                            provides contact centre services with features as calling, messaging, mailing, calling features, 
+                            provides contact centre services with features as calling, messaging, mailing, calling features, 
+                            provides contact centre services with features as calling, messaging, mailing, calling features, 
+                            </p>
+                            }
+                            <br/><br/> <hr/><br/><br/>
+
+                            <span className="faqStyletitle" onClick={()=>this.toggleDiv("show6")}>6. WHERE IS RECALLO MOBILE BASED?</span>
+                            {
+                               show6 ?
+                               <div onClick={()=>this.toggleDiv("show6")} className="minus-icon " />
+                               :
+                               <div onClick={()=>this.toggleDiv("show6")} className="plus-icon " />
+
+                             }
+
+
+
+                            {show6 &&
+                            <p className="faqStyletext"> 
+                            provides contact centre services with features as calling, messaging, mailing, calling features, 
+                            provides contact centre services with features as calling, messaging, mailing, calling features, 
+                            provides contact centre services with features as calling, messaging, mailing, calling features, 
+                            </p>
+                            }
+                            <br/><br/> <hr/><br/><br/>
+                         </div> 
+               
+                    </div>
+                   
+                         }
       </>
     );
   }
